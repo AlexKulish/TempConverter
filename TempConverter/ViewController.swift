3ec5e3dc107304367cf3288aac969998dcc39ca1
Alexey Kulish <alexkulish1993@gmail.com>
2022-05-25 17:54:59 +0300

    Initial Commit

---
+++ TempConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TempConverter
+//
+//  Created by Alex Kulish on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
