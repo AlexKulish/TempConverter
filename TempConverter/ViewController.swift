511b1473b4fcb294f05d8588f676e3cf4b6afebe
Alexey Kulish <alexkulish1993@gmail.com>
2022-05-26 11:41:47 +0300

    add logic for changed celsius and fahrenheit

--- TempConverter/ViewController.swift
+++ TempConverter/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var celciusLabel: UILabel!
+    @IBOutlet weak var farenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = -100
+            slider.value = 0
+            slider.minimumTrackTintColor = .yellow
+            slider.thumbTintColor = .orange
+        }
     }
-
-
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let celsiusTemperature = Int(round(sender.value))
+        celciusLabel.text = "\(celsiusTemperature)ºC"
+        let farenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        farenheitLabel.text = "\(farenheitTemperature)ºF"
+    }
+    
 }
 
